5fdcbb083296244a3c1e3746163f4ead7203b85a
aristonhs <ariston83@gmail.com>
2016-03-25 18:51:00 +0700

    SetDatabaseLocation Update
    not yet final but please check

--- _CODE_/RoyalPetz_ADMIN/RoyalPetz_ADMIN/setDatabaseLocationForm.Designer.cs
+++ _CODE_/RoyalPetz_ADMIN/RoyalPetz_ADMIN/setDatabaseLocationForm.Designer.cs
@@ -28,38 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.serverIPRadioButton = new System.Windows.Forms.RadioButton();
             this.ipAddressMaskedTextbox = new System.Windows.Forms.MaskedTextBox();
+            this.serverIPRadioButton = new System.Windows.Forms.RadioButton();
             this.localhostRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.saveButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 88.0597F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.9403F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 354F));
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.localhostRadioButton, 2, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 36);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(487, 113);
-            this.tableLayoutPanel1.TabIndex = 15;
             // 
             // label2
             // 
@@ -67,37 +46,23 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(3, 23);
+            this.label2.Location = new System.Drawing.Point(12, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 18);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "DATABASE";
+            this.label2.Size = new System.Drawing.Size(238, 18);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Setting Database Location";
             // 
-            // label4
+            // ipAddressMaskedTextbox
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(120, 23);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(9, 18);
-            this.label4.TabIndex = 10;
-            this.label4.Text = ":";
-            // 
-            // tableLayoutPanel2
-            // 
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.serverIPRadioButton, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.ipAddressMaskedTextbox, 0, 1);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(135, 3);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 59);
-            this.tableLayoutPanel2.TabIndex = 11;
+            this.ipAddressMaskedTextbox.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.ipAddressMaskedTextbox.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipAddressMaskedTextbox.Location = new System.Drawing.Point(27, 72);
+            this.ipAddressMaskedTextbox.Mask = "000.000.000.000";
+            this.ipAddressMaskedTextbox.Name = "ipAddressMaskedTextbox";
+            this.ipAddressMaskedTextbox.Size = new System.Drawing.Size(149, 27);
+            this.ipAddressMaskedTextbox.TabIndex = 20;
+            this.ipAddressMaskedTextbox.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.ipAddressMaskedTextbox.Visible = false;
             // 
             // serverIPRadioButton
             // 
@@ -105,69 +70,83 @@
             this.serverIPRadioButton.AutoSize = true;
             this.serverIPRadioButton.Checked = true;
             this.serverIPRadioButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.serverIPRadioButton.Location = new System.Drawing.Point(3, 3);
+            this.serverIPRadioButton.Location = new System.Drawing.Point(6, 50);
             this.serverIPRadioButton.Name = "serverIPRadioButton";
             this.serverIPRadioButton.Size = new System.Drawing.Size(110, 22);
             this.serverIPRadioButton.TabIndex = 19;
             this.serverIPRadioButton.TabStop = true;
             this.serverIPRadioButton.Text = "Server IP";
             this.serverIPRadioButton.UseVisualStyleBackColor = true;
-            this.serverIPRadioButton.CheckedChanged += new System.EventHandler(this.serverIPRadioButton_CheckedChanged);
-            this.serverIPRadioButton.Click += new System.EventHandler(this.serverIPRadioButton_Click);
-            // 
-            // ipAddressMaskedTextbox
-            // 
-            this.ipAddressMaskedTextbox.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
-            this.ipAddressMaskedTextbox.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ipAddressMaskedTextbox.Location = new System.Drawing.Point(3, 32);
-            this.ipAddressMaskedTextbox.Mask = "000.000.000.000";
-            this.ipAddressMaskedTextbox.Name = "ipAddressMaskedTextbox";
-            this.ipAddressMaskedTextbox.Size = new System.Drawing.Size(149, 27);
-            this.ipAddressMaskedTextbox.TabIndex = 20;
-            this.ipAddressMaskedTextbox.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.serverIPRadioButton.CheckedChanged += new System.EventHandler(this.serverIPRadioButton_CheckedChanged_1);
             // 
             // localhostRadioButton
             // 
             this.localhostRadioButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.localhostRadioButton.AutoSize = true;
             this.localhostRadioButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.localhostRadioButton.Location = new System.Drawing.Point(135, 78);
+            this.localhostRadioButton.Location = new System.Drawing.Point(6, 22);
             this.localhostRadioButton.Name = "localhostRadioButton";
-            this.localhostRadioButton.Size = new System.Drawing.Size(134, 22);
+            this.localhostRadioButton.Size = new System.Drawing.Size(174, 22);
             this.localhostRadioButton.TabIndex = 20;
             this.localhostRadioButton.TabStop = true;
-            this.localhostRadioButton.Text = "LOCALHOST";
+            this.localhostRadioButton.Text = "Local Connection";
             this.localhostRadioButton.UseVisualStyleBackColor = true;
-            this.localhostRadioButton.CheckedChanged += new System.EventHandler(this.localhostRadioButton_CheckedChanged);
-            this.localhostRadioButton.Click += new System.EventHandler(this.localhostRadioButton_Click);
+            this.localhostRadioButton.CheckedChanged += new System.EventHandler(this.localhostRadioButton_CheckedChanged_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ipAddressMaskedTextbox);
+            this.groupBox1.Controls.Add(this.serverIPRadioButton);
+            this.groupBox1.Controls.Add(this.localhostRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(21, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 105);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
             // 
             // panel1
             // 
-            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.errorLabel);
             this.panel1.Location = new System.Drawing.Point(1, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(487, 29);
-            this.panel1.TabIndex = 18;
+            this.panel1.Size = new System.Drawing.Size(260, 29);
+            this.panel1.TabIndex = 22;
             // 
             // saveButton
             // 
             this.saveButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.saveButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveButton.Location = new System.Drawing.Point(190, 164);
+            this.saveButton.Location = new System.Drawing.Point(77, 172);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(108, 37);
-            this.saveButton.TabIndex = 16;
+            this.saveButton.TabIndex = 21;
             this.saveButton.Text = "SAVE";
             this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click_1);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BackColor = System.Drawing.Color.White;
+            this.errorLabel.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(3, 6);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(23, 18);
+            this.errorLabel.TabIndex = 26;
+            this.errorLabel.Text = "   ";
             // 
             // setDatabaseLocationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FloralWhite;
-            this.ClientSize = new System.Drawing.Size(489, 215);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(262, 216);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.saveButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -178,24 +157,24 @@
             this.Text = "Pengaturan Lokasi Database";
             this.Activated += new System.EventHandler(this.setDatabaseLocationForm_Activated);
             this.Load += new System.EventHandler(this.setDatabaseLocationForm_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.RadioButton serverIPRadioButton;
         private System.Windows.Forms.MaskedTextBox ipAddressMaskedTextbox;
+        private System.Windows.Forms.RadioButton serverIPRadioButton;
         private System.Windows.Forms.RadioButton localhostRadioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label errorLabel;
     }
 }
--- _CODE_/RoyalPetz_ADMIN/RoyalPetz_ADMIN/setDatabaseLocationForm.cs
+++ _CODE_/RoyalPetz_ADMIN/RoyalPetz_ADMIN/setDatabaseLocationForm.cs
@@ -7,46 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace RoyalPetz_ADMIN
 {
     public partial class setDatabaseLocationForm : Form
     {
         private globalUtilities gutil = new globalUtilities();
+        string appPath = Application.StartupPath;
+        string selectedIP = "";
+
         public setDatabaseLocationForm()
         {
             InitializeComponent();
         }
 
-        private void serverIPRadioButton_CheckedChanged(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void localhostRadioButton_CheckedChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void serverIPRadioButton_Click(object sender, EventArgs e)
         {
-            if (serverIPRadioButton.Checked)
+            /*if (serverIPRadioButton.Checked)
             {
-                ipAddressMaskedTextbox.Enabled = true;
+                //ipAddressMaskedTextbox.Enabled = true;
                 localhostRadioButton.Checked = false;
                 serverIPRadioButton.Checked = true;
             }
             else
             {
-                ipAddressMaskedTextbox.Enabled = false;
+                //ipAddressMaskedTextbox.Enabled = false;
                 localhostRadioButton.Checked = true;
                 serverIPRadioButton.Checked = false;
-            }
+            }*/
         }
 
         private void localhostRadioButton_Click(object sender, EventArgs e)
         {
-            if (localhostRadioButton.Checked)
+            /*if (localhostRadioButton.Checked)
             {
                 ipAddressMaskedTextbox.Enabled = false;
                 localhostRadioButton.Checked = true;
@@ -57,7 +51,7 @@ namespace RoyalPetz_ADMIN
                 ipAddressMaskedTextbox.Enabled = true;
                 localhostRadioButton.Checked = false;
                 serverIPRadioButton.Checked = true;
-            }
+            }*/
         }
 
         private void setDatabaseLocationForm_Load(object sender, EventArgs e)
@@ -65,9 +59,108 @@ namespace RoyalPetz_ADMIN
             gutil.reArrangeTabOrder(this);
         }
 
+        private void createConfig(String IPAdrs)
+        {
+            //default config file name is pos.cfg
+            string path = appPath + "\\pos.cfg";
+
+            File.WriteAllText(path, IPAdrs);
+            MessageBox.Show("Update Success!");
+            /*if (!File.Exists(@path))
+            {
+                File.Create(@path);
+                TextWriter tw = new StreamWriter(@path,true);
+
+                //assume localconnection if IPAdrs is error
+                tw.WriteLine(IPAdrs);
+                tw.Close();
+            }
+            else if (File.Exists(@path))
+            {
+                using (var fs = new FileStream(@path, FileMode.Truncate))
+                {
+                }
+                TextWriter tw = new StreamWriter(@path);
+                tw.WriteLine(IPAdrs);
+                tw.Close();
+            }*/
+        }
+
+        private bool checkconfig()
+        {
+            bool rslt = false;
+            string installpath = appPath + "\\pos.cfg";
+            String tmp = "";
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                
+                using (StreamReader sr = new StreamReader(installpath))
+                {
+                    string line;
+
+                    // Read and display lines from the file until 
+                    // the end of the file is reached. 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        tmp = tmp + line;
+                    }
+                }
+                rslt = true;
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                errorLabel.Text = "File Config Not Found";
+                rslt = false;
+            }
+            finally
+            {
+                selectedIP = tmp.Trim();
+            }
+            return rslt;
+        }
+
         private void setDatabaseLocationForm_Activated(object sender, EventArgs e)
         {
             //if need something
+
+            if (checkconfig())
+            {
+                if (selectedIP.Equals("localhost"))
+                {
+                    localhostRadioButton.PerformClick();
+                }
+                else
+                {
+                    serverIPRadioButton.PerformClick();
+                    ipAddressMaskedTextbox.Text = selectedIP;
+                }
+            }
+        }
+
+        private void saveButton_Click_1(object sender, EventArgs e)
+        {
+            //save to setting 
+            if (serverIPRadioButton.Checked)
+            {
+                createConfig(ipAddressMaskedTextbox.Text);
+            } else
+            {
+                createConfig("localhost");
+            }
+        }
+
+        private void localhostRadioButton_CheckedChanged_1(object sender, EventArgs e)
+        {
+            ipAddressMaskedTextbox.Visible = false;
+            ipAddressMaskedTextbox.Clear();
+        }
+
+        private void serverIPRadioButton_CheckedChanged_1(object sender, EventArgs e)
+        {
+            ipAddressMaskedTextbox.Visible = true;
         }
     }
 }
